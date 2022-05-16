cf3507b16c455c1dec641d10cc6872b9ab38c031
정지은 <52345872+FANOOJungjieun@users.noreply.github.com>
2022-05-16 14:14:33 +0900

    Create 프로그래머스_n^2배열자르기.py
    divmod : 몫과 나머지를 동시에 구하는 함수

---
+++ 프로그래머스_n^2배열자르기.py
@@ -0,0 +1,10 @@
+def solution(n, left, right):
+    answer = []
+    
+    for i in range(left, right+1) :
+        q, r = divmod(i,n)
+        if q > r :
+            answer.append(q+1)
+        else : 
+            answer.append(r+1)
+    return answer
