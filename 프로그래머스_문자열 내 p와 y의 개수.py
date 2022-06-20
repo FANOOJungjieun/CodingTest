9e3caef98f1b79679aad444a72042d9bf0e573c1
정지은 <52345872+FANOOJungjieun@users.noreply.github.com>
2022-06-20 14:05:54 +0900

    Create 프로그래머스_문자열 내 p와 y의 개수.py

---
+++ 프로그래머스_문자열 내 p와 y의 개수.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = True
+    
+    s = s.upper()
+    p_count = s.count('P')
+    y_count = s.count('Y')
+    
+    if p_count == y_count :
+        return True
+    else:
+        return False
+
+    return True
