6341db4c808dad91cad43cf3396578dbdc89889b
정지은 <52345872+FANOOJungjieun@users.noreply.github.com>
2022-08-11 13:57:53 +0900

    Create Leetcode_1137. N-th Tribonacci Number.cpp

---
+++ Leetcode_1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,19 @@
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for N-th Tribonacci Number.
+// Memory Usage: 6 MB, less than 51.66% of C++ online submissions for N-th Tribonacci Number.
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        int dp[38] = {0};
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        
+        return dp[n];
+    }
+};
