7cddacc1f39124688339a522b511e426b34477fd
정지은 <52345872+FANOOJungjieun@users.noreply.github.com>
2022-09-06 15:56:12 +0900

    Create Leetcode_29. Divide Two Integers.py

---
+++ Leetcode_29. Divide Two Integers.py
@@ -0,0 +1,23 @@
+# bit shift연산
+
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+
+        sign = 1
+		# Storing sign if any one if negative 
+        if (dividend < 0) ^ (divisor < 0) :
+            sign = -1
+            
+        dividend = abs(dividend)
+        divisor = abs (divisor)
+        temp = quotient = 0
+        for i in range(31, -1, -1):
+            if temp + (divisor << i) <= dividend:
+                temp += divisor << i
+                quotient += 1 << i
+        if sign == -1:
+            quotient = -quotient
+		#handling if goes out of range i.e (2**(31))-1
+        if abs(quotient) > (1 << 31 - 1) and quotient > 0 :
+            quotient = (1 << 31) - 1
+        return quotient
