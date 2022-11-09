dd0e43e4992c3a7de33faba41a317ea06ddfd1cd
정지은 <52345872+FANOOJungjieun@users.noreply.github.com>
2022-11-09 13:45:12 +0900

    Create Leetcode_901. Online Stock Span.py

---
+++ Leetcode_901. Online Stock Span.py
@@ -0,0 +1,19 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.l = []
+
+    def next(self, price: int) -> int:
+        ans = 1
+        
+        while self.l and self.l[-1][0] <= price:
+            ans += self.l.pop()[1]
+        
+        self.l.append([price,ans])
+        
+        return ans
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
